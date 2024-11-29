d1abd4ee58e6fcabaf6da60ac92ca9bd355fd37d
VanshMehta588 <vanshmehta873@gmail.com>
2024-11-29 19:39:18 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -2,14 +2,48 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import LoadingBar from 'react-top-loading-bar'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
 
 export default class App extends Component {
+  // apiKey = "b36a3b1d74f04ceaaaebca7da4a778fb";
+  // apiKey = "7a7d92e077be45eaa5badcb4941dcba7";
+  apiKey = process.env.REACT_APP_API_CODE;
+  pageSize = 6;
+  state = {
+    progress: 0
+  }
+  setProgress = (progress) => {
+    this.setState({ progress: progress })
+  }
   render() {
     return (
       <>
-        <Navbar />
-        <News />
+
+        <Router>
+          <div className='customContainer'>
+            <Navbar />
+            <LoadingBar color='#d1dfdc' progress={this.state.progress} />
+            <Routes>
+              <Route path="/" element={<News setProgress={this.setProgress} pageSize={this.pageSize} apiKey={this.apiKey} category="general" country="us" />} />
+              <Route path="/business" element={<News setProgress={this.setProgress} pageSize={this.pageSize} apiKey={this.apiKey} category="business" country="us" />} />
+              <Route path="/entertainment" element={<News setProgress={this.setProgress} pageSize={this.pageSize} apiKey={this.apiKey} category="entertainment" country="us" />} />
+              <Route path="/health" element={<News setProgress={this.setProgress} pageSize={this.pageSize} apiKey={this.apiKey} category="health" country="us" />} />
+              <Route path="/science" element={<News setProgress={this.setProgress} pageSize={this.pageSize} apiKey={this.apiKey} category="science" country="us" />} />
+              <Route path="/sports" element={<News setProgress={this.setProgress} pageSize={this.pageSize} apiKey={this.apiKey} category="sports" country="us" />} />
+              <Route path="/technology" element={<News setProgress={this.setProgress} pageSize={this.pageSize} apiKey={this.apiKey} category="technology" country="us" />} />
+            </Routes>
+            {/* <News setProgress = {setProgress} pageSize={20} apiKey={"7a7d92e077be45eaa5badcb4941dcba7"} category={"general"} country={"us"}/> */}
+          </div>
+        </Router>
       </>
     )
   }
 }
+
--- src/components/NewItems.js
+++ src/components/NewItems.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewItems extends Component {
-    render() {
-        let { title, description, imageURL, newsURL} = this.props;
-        return (
-            <>
-                <div className="card" style={{ width: "auto" }}>
-                    <img src={imageURL} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">
-                            {description}...
-                        </p>
-                        <a href={newsURL} target='_blank' className="btn btn-sm btn-info">
-                            Read More
-                        </a>
-                    </div>
+const NewItems = (props) => {
+    let { title, description, imageURL, newsURL, author, date, source } = props;
+    return (
+        <>
+            <div className="card">
+                <img src={imageURL} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}...</h5>
+                    <p className="card-text">
+                        {description}...
+                    </p>
+                    <h6 className='card-text'> <span className="badge text-bg-warning">{source}</span></h6>
+                    <p className="card-text"><small className="text-body-secondary">By {author} <br />Published Date : {new Date(date).toGMTString()}</small></p>
+                    <a href={newsURL} target='_blank' className="btn btn-sm btn-info">
+                        Read More
+                    </a>
                 </div>
+            </div>
 
-            </>
-        )
-    }
+        </>
+    )
 }
 
 export default NewItems
